f6cb58d529d40d47ba39d71ecc60a2499ca675fe
Marian P. Gajda <mail@marian-gajda.com>
2018-04-12 12:02:16 +0200

    removed build directory of documentation

--- doc/build/html/_static/documentation_options.js
+++
@@ -1,9 +0,0 @@
-var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: '',
-    VERSION: '10.04.2018',
-    LANGUAGE: 'de',
-    COLLAPSE_INDEX: false,
-    FILE_SUFFIX: '.html',
-    HAS_SOURCE: true,
-    SOURCELINK_SUFFIX: '.txt'
-};
