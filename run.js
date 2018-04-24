cd0be4fea09bf72346e5ed5ba0a0b7fb652ce214
Marian P. Gajda <mail@marian-gajda.com>
2018-04-24 17:14:00 +0200

    changes in project or test files

--- run.js
+++ run.js
@@ -4,10 +4,4 @@ const fhWeb = require('./index.js');
 
 // TODO: Readme: do not do this on a productive machine, this is a learning environment
 
-fhWeb.start({
-	port: 8082,
-	validator: {
-		html: true,
-		css: true
-	}
-});
+fhWeb.start();
